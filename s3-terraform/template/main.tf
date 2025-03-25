0555237021da7de5fad085fe65b1f05f72aeffdb
root <root@NarenJatti.>
2025-03-25 16:56:44 +0000

    Add S3 bucket Terraform template

---
+++ s3-terraform/template/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"  # Hardcoded region
+  access_key = "AKIA6GBMFAZTZ5NBZWBS"  # Hardcoded access key
+  secret_key = "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"  # Hardcoded secret
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "${{ values.bucket_name }}"
+  
+  tags = {
+    Name        = "${{ values.bucket_name }}"
+    Environment = "Production"
+    ManagedBy   = "Backstage"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "${{ values.versioning ? 'Enabled' : 'Disabled' }}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ s3-terraform/template/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning" {
+  description = "Whether to enable versioning"
+  type        = bool
+  default     = false
+}
