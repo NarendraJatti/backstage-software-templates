9403ce133c0b45badef9b902a586ad559422a662
Narendra Jatti <narendra.jatti22@gmail.com>
2025-03-25 22:54:55 +0530

    Update main.tf

--- s3-terraform/template/main.tf
+++ s3-terraform/template/main.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket" "this" {
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
-    status = "${{ values.versioning ? 'Enabled' : 'Disabled' }}"
+  status = "${{ values.versioning ? 'Enabled' : 'Disabled' }}"
   }
 }
 
