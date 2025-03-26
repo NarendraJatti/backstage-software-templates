c038ab6c1623896cc56324577bed01183ee873a4
Narendra Jatti <narendra.jatti22@gmail.com>
2025-03-26 14:22:17 +0530

    Update variables.tf

--- s3-terraform/template/variables.tf
+++ s3-terraform/template/variables.tf
@@ -1,16 +1,16 @@
 variable "bucket_name" {
-  description = "Name of the S3 bucket"
+  description = "Name of the S3 bucket (must be globally unique)"
   type        = string
 }
 
 variable "region" {
-  description = "AWS region"
+  description = "AWS region where resources will be created"
   type        = string
-  default     = "us-east-1"  # Added default value
+  default     = "us-east-1"
 }
 
 variable "versioning_enabled" {
-  description = "Whether to enable versioning"
+  description = "Enable versioning for the S3 bucket"
   type        = bool
   default     = false
 }
