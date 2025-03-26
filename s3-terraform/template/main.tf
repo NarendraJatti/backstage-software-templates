bd83a4816396cb56474ccec6d51148fe081a17eb
Narendra Jatti <narendra.jatti22@gmail.com>
2025-03-26 15:08:47 +0530

    Update main.tf

--- s3-terraform/template/main.tf
+++ s3-terraform/template/main.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3IJ74JHWC62ZBTCH"       # ⚠️ Remove in production
-  secret_key = "ZWGpVFK43DzIob8KEIAbpK6T99XzLmCmGVXFC9RG" # ⚠️ Remove in production
+  access_key = "AKIA3IJ74JHWC62ZBTCH"
+  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "this" {
