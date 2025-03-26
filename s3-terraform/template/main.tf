bf1e0ec336648ab70059fe4633d7f814de26b87f
Narendra Jatti <narendra.jatti22@gmail.com>
2025-03-26 15:41:02 +0530

    Update main.tf

--- s3-terraform/template/main.tf
+++ s3-terraform/template/main.tf
@@ -13,8 +13,26 @@ provider "aws" {
   secret_key = "ZWGpVFK43DzIob8KEIAbpK6T99XzLmCmGVXFC9RG"
 }
 
-data "aws_s3_buckets" "current" {}
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name-${formatdate("YYYYMMDDhhmmss", timestamp())}"  
+  # Using timestamp to ensure unique name
 
-output "bucket_list" {
-  value = data.aws_s3_buckets.current.buckets[*].name
+  tags = {
+    Name        = "MyBackstageBucket"
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
 }
